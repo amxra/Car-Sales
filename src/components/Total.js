6f209b896c3ab7602888404d74790932770c578c
amxra <amiradediran@gmail.com>
2019-10-10 00:14:32 +0100

    import connect and create connected version of component and export it

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * from '../state/actionCreators';
 
-const Total = props => {
+export const Total = props => {
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
@@ -10,3 +11,8 @@ const Total = props => {
 };
 
 export default Total;
+
+export default connect(
+  state => state,
+  actionCreators,
+)(Total);
